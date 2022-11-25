803cdf6e4923aaee687c0105a7b199f45e73cab1
nvor <nickbaripour@gmail.com>
2022-11-24 23:57:54 -0500

    args

--- pkg/args_proc/args_processor.go
+++ pkg/args_proc/args_processor.go
@@ -2,11 +2,32 @@ package args_proc
 
 import (
 	"log"
-	"os"
+	//"os"
 )
+
+type Command struct {
+	url string
+	mode string
+	options []string
+}
 
 func ProcessArgs(args []string) string {
 	log.Println("Processing arguments:", args)
+
+	log.Println("size", len(args))
+	if len(args) < 1 || len(args) > 2 {
+		log.Println("Error: Invalid number of arguments")
+		return "replace with error - exit program"
+	}
+
+	log.Println("DS", args[0])
+
+	for i := 0; i < len(args); i++ {
+		if i == 0 {
+			processUrlArg(args[i])
+		}
+	}
+
 	var url string
 
 	if args[0] != "" {
@@ -17,4 +38,12 @@ func ProcessArgs(args []string) string {
 	}
 
 	return url
+}
+
+func processUrlArg(urlArg string) string {
+	log.Println("process url")
+}
+
+func processModeArg(modeArg string) {
+	log.Println("process mode")
 }
